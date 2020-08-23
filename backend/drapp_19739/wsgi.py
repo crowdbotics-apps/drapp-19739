8d78e8d4b6a48f7711dbfa38e58055609d86736d
Crowdbotics <team@crowdbotics.com>
2020-08-23 11:05:15 +0000

    Update models

--- backend/drapp_19739/wsgi.py
+++ backend/drapp_19739/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drapp_19739.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drapp_19739.settings")
 
 application = get_wsgi_application()
